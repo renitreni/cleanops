e35b70b508784afb5cf175fbb37491a33c5d20eb
unknown <renier.trenuela@gmail.com>
2025-02-25 18:17:24 +0800

    fix redirect url

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->to('https://sys.aljouf-baladiya.site/');
+    return redirect()->to('https://aljouf-baladiya.site/');
 });
