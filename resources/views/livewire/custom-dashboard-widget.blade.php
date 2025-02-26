94f7222a09ab1a58f182575885152ce082bad21e
unknown <renier.trenuela@gmail.com>
2025-02-26 19:27:01 +0800

    update

--- resources/views/livewire/custom-dashboard-widget.blade.php
+++ resources/views/livewire/custom-dashboard-widget.blade.php
@@ -1,6 +1,10 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-        <h2 class="text-2xl font-bold text-red-800 animate-pulse text-red">Reports has been due</h2>
-        <p class="text-red-200 animate-pulse">Please see your assigned task that are due.</p>
+        <h2 class="text-2xl font-bold animate-pulse text-red" style="
+        color: red;
+    ">Reports has been due</h2>
+        <p class=" animate-pulse" style="
+        color: red;
+    ">Please see your assigned task that are due.</p>
     </x-filament::section>
 </x-filament-widgets::widget>
