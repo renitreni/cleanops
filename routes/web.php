b8cae13d759d72fe2f172ceba3860a4339c20b7f
renierdev <renier.trenuela@gmail.com>
2025-03-02 11:32:13 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->to('https://aljouf-baladiya.site/');
+    return '';
 });
-
-
-
-
 
 Route::get('/complaint-report', function () {
     return view('complaint-report');
-
 });
