21ec6569af65622eb7d45798106de17bb88ccf6a
unknown <renier.trenuela@gmail.com>
2025-03-04 11:02:22 +0800

    update theme

--- resources/css/filament/portal/tailwind.config.js
+++ resources/css/filament/portal/tailwind.config.js
@@ -3,6 +3,11 @@ import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
 export default {
     presets: [preset],
     content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
         './app/Filament/Portal/**/*.php',
         './resources/views/filament/portal/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
