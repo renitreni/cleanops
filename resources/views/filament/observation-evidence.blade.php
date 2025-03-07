ab07126ef6d1b02e085deabff328af0f09adc4da
unknown <renier.trenuela@gmail.com>
2025-03-07 08:54:07 +0800

    update layout

--- resources/views/filament/observation-evidence.blade.php
+++ resources/views/filament/observation-evidence.blade.php
@@ -1,17 +1,16 @@
 <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
     @foreach ($evidences ?? [] as $item)
         @if ($item)
-            <div class="h-32 rounded-lg bg-gray-200">
+            <div class="">
                 @if (Str::contains($item, '.mp4'))
                     <video width='420' height='240' controls>
                         <source src='{{ $item }}' type=''>
                         Your browser does not support the video tag.
                     </video>
                 @else
-                    <img src='{{ $item }}' target='_blank' width='420' />
+                    <img src='{{ $item }}' target='_blank' width='420' height='240'/>
                 @endif
             </div>
         @endif
     @endforeach
-
 </div>
