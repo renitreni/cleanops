640b3db165812537229a8e8c5277f562ec1c9d12
unknown <renier.trenuela@gmail.com>
2025-03-07 11:05:20 +0800

    design update

--- resources/views/filament/custom-logo.blade.php
+++ resources/views/filament/custom-logo.blade.php
@@ -1,1 +1,2 @@
-<img src="https://ajmalsa.com/wp-content/uploads/2025/02/inma-logo-removebg-preview.png" alt="inma-logo">
+<img src="https://ajmalsa.com/wp-content/uploads/2025/02/inma-logo-removebg-preview.png"
+    style="background-color: #a52a2ae6;padding: 7px;border-radius: 5px;" alt="inma-logo">
