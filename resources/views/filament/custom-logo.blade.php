7506a12700841d187607b9d19896941fb4956420
renier <renier.trenuela@gmail.com>
2025-03-08 10:54:13 +0800

    updating imagr in logo

--- resources/views/filament/custom-logo.blade.php
+++ resources/views/filament/custom-logo.blade.php
@@ -1,2 +1,2 @@
 <img src="https://ajmalsa.com/wp-content/uploads/2025/02/inma-logo-removebg-preview.png"
-    style="background-color: #a52a2ae6;padding: 7px;border-radius: 5px;" alt="inma-logo">
+    style="background-image: url('{{ asset('images/blurry_background.png') }}'); background-size: cover; padding: 7px;border-radius: 8px;" alt="inma-logo">
